e178530a10369530f9fadc8aec27f46597baf03e
sindre2 <106583791+sindre2@users.noreply.github.com>
2022-08-15 11:20:17 +0200

    Main files for skeletor soundcard

---
+++ juvlaskript.js
@@ -0,0 +1,85 @@
+
+/* --- Global constants, arrays and an object for keyboard buttons. ---*/
+
+const drumBtn = [];
+const audioList = [];
+const skeletor = ["./Sound/Explode.mp3", "./Sound/Fights.mp3", "./Sound/Laugh.mp3", "./Sound/Myah.mp3", "./Sound/Nice.mp3", "./Sound/Not-kind.mp3", "./Sound/Stop-licking.mp3", "./Sound/Witless-fool.mp3"];
+const keyboard = 
+    {
+        "1" : 0,
+        "2" : 1,
+        "3" : 2,
+        "4" : 3,
+        "5" : 4,
+        "6" : 5,
+        "7" : 6,
+        "8" : 7
+    };
+
+/* --- END --- */
+
+
+
+/* --- Function to make an array for drumBtns and run said function. --- */
+
+function makeVariables ()
+    {
+        for (let x = 0; x < 8; x++)
+            {
+                drumBtn[x] = document.getElementById(`btn-${x + 1}`);
+            }
+        return drumBtn;
+    };
+
+makeVariables();
+
+/* --- END --- */
+
+
+
+/* --- forEach method to map the entire button array with event listeners that play sound. --- */
+
+skeletor.forEach((filename, index) =>
+    {
+        audioList[index] = new Audio (filename);
+        drumBtn[index].textContent = filename.slice(8, -4);
+
+        drumBtn[index].addEventListener("click",() => 
+        {
+            audioList[index].pause();
+            audioList[index].play();
+        })
+    });
+
+/* --- END --- */
+
+
+
+/* --- Event listeners for the keyboard buttons 1 - 8. The ".key" method converts the input from the keyboard into a string, then locates the value from the "keyboard" - object. ---*/
+
+document.addEventListener("keydown", function(input)    
+    {
+        console.log(input.key);
+
+        if (keyboard[input.key] === 0)
+        {
+            playSound(input.key);
+        } else (keyboard[input.key])
+        {
+            playSound(input.key);
+        }
+    });
+
+/* --- END --- */ 
+    
+    
+
+/* --- Function to play sound --- */
+
+function playSound (input)
+{
+    audioList[keyboard[input]].pause();
+    audioList[keyboard[input]].play();
+};
+
+/* --- END --- */
